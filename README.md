51d2e1f5fd54a0127d5163806d6e2d583db7201a
Slava Kim <slava@collections.me>
2013-01-19 13:32:53 -0800

    Some minor code style fixes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 My dev blog
 ===
 
-So what I did, I just forked [https://github.com/brianleroux/wtfjs][wtfjs] repository and tweeked a bit for my own purposes.
+So what I did, I just forked [https://github.com/brianleroux/wtfjs](wtfjs) repository and tweeked a bit for my own purposes.
--- server.js
+++ server.js
@@ -3,18 +3,17 @@ var post = require('./lib/post')
 ,   fs   = require('fs')
 ,   path = require('path')
 ,   app  = require('./lib/config').app
-,   PORT = process.env.PORT || 4000
-,   LICENSE = fs.readFileSync(path.join(__dirname, 'LICENSE'))
+,   PORT = process.env.PORT || 4000;
 
 // GET "/" - lists first 5 get("/", Post.paginate);
 app.get('/', function(req, res) {
-    res.render('index.ejs', Post.page(1))
-})
+    res.render('index.ejs', Post.page(1));
+});
 
 // GET "/page/2" - lists 5 posts for the page passed
 app.get("/page/:n", function(req, res) {
-   res.render('index.ejs', Post.page(req.params.n))
-})
+   res.render('index.ejs', Post.page(req.params.n));
+});
 
 // GET "/2010/05/10/title-of-article" - post permalink
 app.get("/:y/:m/:d/:title", function(req, res) {
@@ -22,26 +21,21 @@ app.get("/:y/:m/:d/:title", function(req, res) {
     ,   m = req.params.m
     ,   d = req.params.d
     ,   t = req.params.title
-    ,   p = new Post([y,m,d,t].join('-') + '.md')
-    res.render("post.ejs", {post:p})
-})
+    ,   p = new Post([y,m,d,t].join('-') + '.md');
+    res.render("post.ejs", {post:p});
+});
 
 // GET "/rss"
 app.get("/rss", function(req, res) {    
-    res.type('text/xml')
-    res.charset = 'utf-8'
-    res.send(Post.rss())
-})
-
-// GET "/license" - diplays the WTFPL
-app.get('/license', function(req, res) {
-    res.send(LICENSE)
-})
+    res.type('text/xml');
+    res.charset = 'utf-8';
+    res.send(Post.rss());
+});
 
 // GET "/about"
 app.get("/about", function(req, res) {
-    res.render('about.ejs')
-})
+    res.render('about.ejs');
+});
 
-console.log("server.js listening on http://localhost:"+PORT)
-app.listen(PORT)
+console.log("server.js listening on http://localhost:"+PORT);
+app.listen(PORT);
